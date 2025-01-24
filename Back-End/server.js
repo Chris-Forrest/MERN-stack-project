0ec34eed05d1ea792d91680daddab45431364988
Chris Forrest <42876707+Chris-Forrest@users.noreply.github.com>
2025-01-24 11:45:53 -0500

    Update server.js
    made initial get route to test nodemon and server is working

--- Back-End/server.js
+++ Back-End/server.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send("server is working")
+})
+
 app.listen(5000, () => {
     console.log("Server started at http://localhost:5000");
 });
