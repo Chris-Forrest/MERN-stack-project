06bc9c94205339abd46c4d74943f796ab8b1253f
Chris Forrest <42876707+Chris-Forrest@users.noreply.github.com>
2025-01-24 12:32:57 -0500

    added .env
    added .env to gitignore and added mongodb connection string

--- Back-End/server.js
+++ Back-End/server.js
@@ -1,11 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("server is working")
-})
+app.get("/products", (req, res) => {});
+
 
 app.listen(5000, () => {
     console.log("Server started at http://localhost:5000");
 });
+
