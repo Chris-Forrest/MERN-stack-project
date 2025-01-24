a515b89c3474c24214a6e929c8e2c5e277f30ff6
Chris Forrest <42876707+Chris-Forrest@users.noreply.github.com>
2025-01-24 12:58:06 -0500

    db config
    added db config and import connectDB but the auth keeps failing

--- Back-End/config/db.js
+++ Back-End/config/db.js
@@ -1,13 +1,14 @@
-import mongose from "mongoose";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-export const connectDB = async ()=> {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
+dotenv.config();
 
-    } catch (error) {
-        console.error(`Error: ${error.message}`);
-        process.exit(1); //code 1 means exit with failure 0 means success
-    }
+export const connectDB = async () => {
+	try {
+		const conn = await mongoose.connect(process.env.MONGO_URI);
+		console.log(`MongoDB Connected: ${conn.connection.host}`);
+	} catch (error) {
+		console.error(`Error: ${error.message}`);
+		process.exit(1); // process code 1 code means exit with failure, 0 means success
+	}
 };
