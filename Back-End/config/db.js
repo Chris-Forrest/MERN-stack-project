f3b660185d2a02ab8783b7bbe93f82e064b0c3e9
Chris Forrest <42876707+Chris-Forrest@users.noreply.github.com>
2025-01-27 20:50:37 -0500

    Update db.js

--- Back-End/config/db.js
+++ Back-End/config/db.js
@@ -5,8 +5,8 @@ dotenv.config();
 
 export const connectDB = async () => {
 	try {
-		const conn = await mongoose.connect(process.env.MONGO_URI);
-		console.log(`MongoDB Connected: ${conn.connection.host}`);
+		const conn = await mongoose.connect(process.env.MONGO_URI);//authentication to mongo is failing
+		console.log(`MongoDB Connected: ${conn.connection.host}`);// bad auth
 	} catch (error) {
 		console.error(`Error: ${error.message}`);
 		process.exit(1); // process code 1 code means exit with failure, 0 means success
