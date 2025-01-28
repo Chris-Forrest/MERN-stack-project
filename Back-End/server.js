df76696881301ca872e1da1382e51d34cdda2309
Chris Forrest <42876707+Chris-Forrest@users.noreply.github.com>
2025-01-28 14:57:30 -0500

    Update server.js
    adding code for delete route will move to appropriate file under routes later

--- Back-End/server.js
+++ Back-End/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import Product from "./models/product.model.js";
+import e from "express";
 
 dotenv.config();
 
@@ -26,6 +27,18 @@ app.post("/api/products", async (req, res) => {
     } catch (error){
         console.error("Error in Create product:", error.message);
         res.status(500).json({ success: false, message: "Server Error"});
+    }
+});
+
+app.delete("/api/products/:id", async (req,res) => {
+    const {id} = req.params;
+    
+    try {
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({ success: true, message: "Product Deleted"});
+    }catch(error){
+        console.error("Error deleting product:",error.message);
+        res.status(404).json({ success: false, message: "Product not found"});
     }
 });
 
