7ab772a14db11e9bb6530ca0908b7190e3a2e797
Chris Forrest <42876707+Chris-Forrest@users.noreply.github.com>
2025-01-30 09:02:22 -0500

    Update server.js
    got rid of unnecessary imports brought in productRoutes and amended server code to use the route code

--- Back-End/server.js
+++ Back-End/server.js
@@ -1,14 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-import Product from "./models/product.model.js";
-import mongoose from "mongoose";
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api/products", productRoutes);
 
 app.listen(5000, () => {
     connectDB();
