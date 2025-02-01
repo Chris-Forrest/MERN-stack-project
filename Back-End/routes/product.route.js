d23359b3d802d4c62082adfb6400b7e59ecc6bf5
Chris Forrest <42876707+Chris-Forrest@users.noreply.github.com>
2025-02-01 08:28:48 -0500

    put route
    moved code to updateProduct into put.js and updated product.route.js

--- Back-End/routes/product.route.js
+++ Back-End/routes/product.route.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import Product from "../models/product.model.js";
 import { getProducts } from "./get.js";
 import { createProduct } from "./post.js";
+import { updateProduct } from "./put.js";
 
 const router = express.Router();
 
@@ -10,22 +11,7 @@ router.get("/", getProducts);
 
 router.post("/", createProduct);
 
-router.put("/:id", async (req,res) => { 
-    const { id } = req.params;
-
-    const product = req.body;
-
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(404).json({ success: false, message: "Invalid Product Id" });
-    }
-
-    try {
-        const updatedProduct = await Product.findByIdAndUpdate( id, product,{new:true});
-        res.status(200).json({ success: true, data: updatedProduct });
-    } catch (error) {
-        res.status(500).json({ success: false, message: "Server Error"});
-    }
-})
+router.put("/:id", updateProduct);
 
 router.delete("/:id", async (req,res) => { 
     const {id} = req.params;
---
+++ Back-End/routes/put.js
@@ -0,0 +1,18 @@
+import Product from "../models/product.model.js";
+
+export const updateProduct = async (req,res) => { 
+    const { id } = req.params;
+
+    const product = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ success: false, message: "Invalid Product Id" });
+    }
+
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate( id, product,{new:true});
+        res.status(200).json({ success: true, data: updatedProduct });
+    } catch (error) {
+        res.status(500).json({ success: false, message: "Server Error"});
+    }
+};
