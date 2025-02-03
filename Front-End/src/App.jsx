943d1f4d60a3de3ced5a9b67455fa60adfb21aef
Chris Forrest <42876707+Chris-Forrest@users.noreply.github.com>
2025-02-03 10:39:20 -0500

    Update App.jsx
    removed unused code and added initial code for box and Routes

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -1,13 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import { Box, Button } from "@chakra-ui/react";
 
 function App() {
 
   return (
-    <>
-    </>
+    <Box minH={"100vh"}>
+      {/* <Navbar /> */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route Path="/create" element={<CreatePage />} />
+      </Routes>
+    </Box>
   )
 }
 
