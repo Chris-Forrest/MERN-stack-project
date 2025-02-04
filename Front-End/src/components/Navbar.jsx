b1483f994fbf43b90234e987e9dedd979fd51753
Chris Forrest <42876707+Chris-Forrest@users.noreply.github.com>
2025-02-04 09:30:46 -0500

    Update Navbar.jsx
    designing the navbar

--- Front-End/src/components/Navbar.jsx
+++ Front-End/src/components/Navbar.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { Container, Flex, HStack, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <Container maxW={"1140px"} px={4}>
+      <Flex 
+      h={16} 
+      alignItems={"center"} 
+      justifyContent={"space-between"}
+      flexDir={{
+        base:"column",
+        sm:"row"
+      }}
+      >
+        <Text
+            fontSize={{ base: "22", sm:"28"}}
+            fontWeight={"bold"}
+            textTransform={"uppercase"}
+            textAlign={"center"}
+            bgGradient={"linear(to-r, cyan.400, blue.500)"}
+            bgClip={"text"}
+        >
+          <Link to={"/"}>Product Store </Link>
+        </Text>
+        <HStack spacing={2} alignItems={"center"}>
 
-export default Navbar
+          
+        </HStack>
+      </Flex>
+    </Container>
+  );
+};
+
+export default Navbar;
