5dc21f22b04904d61528266e26144d3c5ee07810
Chris Forrest <42876707+Chris-Forrest@users.noreply.github.com>
2025-02-04 10:00:43 -0500

    Update Navbar.jsx
    added light and dark mode imported icons for both and added a shopping cart to the text for product store

--- Front-End/src/components/Navbar.jsx
+++ Front-End/src/components/Navbar.jsx
@@ -1,7 +1,14 @@
-import { Container, Flex, HStack, Text } from "@chakra-ui/react";
+import { Button, Container, Flex, HStack, Text, useColorMode } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
+import { PlusSquareIcon } from "@chakra-ui/icons";
+import { IoMoon } from "react-icons/io5";
+import { LuSun } from "react-icons/lu";
+
 
 const Navbar = () => {
+
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
     <Container maxW={"1140px"} px={4}>
       <Flex 
@@ -21,11 +28,16 @@ const Navbar = () => {
             bgGradient={"linear(to-r, cyan.400, blue.500)"}
             bgClip={"text"}
         >
-          <Link to={"/"}>Product Store </Link>
+          <Link to={"/"}>Product Store 🛒</Link>
         </Text>
         <HStack spacing={2} alignItems={"center"}>
+          <Button>
+            <PlusSquareIcon fontSize={20} />
+            <Button onClick={toggleColorMode}>
+              {colorMode === "light" ? <IoMoon /> : <LuSun size={20}  />}
+            </Button>
+          </Button>
 
-          
         </HStack>
       </Flex>
     </Container>
