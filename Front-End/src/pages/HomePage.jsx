488fc856426fc0fb24b78c567cdc6efbf1683071
Chris Forrest <42876707+Chris-Forrest@users.noreply.github.com>
2025-02-06 13:35:42 -0500

    changes to app
    changes to app and setting up display products need to make the display card

--- Front-End/src/pages/HomePage.jsx
+++ Front-End/src/pages/HomePage.jsx
@@ -10,7 +10,7 @@ const HomePage = () => {
     fetchProducts();
   },[fetchProducts]);
   console.log("products",products)
-  
+
   return (
     <Container maxW="container.xl" py={12}>
       <VStack spacing={8}>
@@ -23,7 +23,19 @@ const HomePage = () => {
         >
           Current Products
         </Text>
-        <SimpleGrid></SimpleGrid>
+        <SimpleGrid
+          columns={{
+            base: 1,
+            md: 2,
+            lg: 3,
+          }}
+          spacing={10}
+          w={"full"}
+        >
+          {products.map((product) => (
+            <ProductCard key={product._id} product={product} /> 
+          ))}
+        </SimpleGrid>
       </VStack>
     </Container>
   )
