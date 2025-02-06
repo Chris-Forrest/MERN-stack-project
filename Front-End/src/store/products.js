73ad0835d0ff490d29d34a34bc74ab5b44ab1ba4
Chris Forrest <42876707+Chris-Forrest@users.noreply.github.com>
2025-02-06 12:59:40 -0500

    Update products.js
    added success message and comma's

--- Front-End/src/store/products.js
+++ Front-End/src/store/products.js
@@ -10,11 +10,12 @@
             const res = await fetch("/api/products", {
                 method: "POST",
                 headers:{
-                    "Content-Type":"application/json"
+                    "Content-Type":"application/json",
                 },
-                body:JSON.stringify(newProduct)
+                body:JSON.stringify(newProduct),
             })
             const data = await res.json();
-            set((state) => ({products:[...state.products, data.data]}))
+            set((state) => ({products:[...state.products, data.data]}));
+            return { success: true, message: "Product created successfully"};
         }
     }));
