d22beb509b2933778f46b0089875302dde05bcd3
Chris Forrest <42876707+Chris-Forrest@users.noreply.github.com>
2025-02-07 13:04:40 -0500

    Update HomePage.jsx
    whenever I add the bgClip text the text disappears

--- Front-End/src/pages/HomePage.jsx
+++ Front-End/src/pages/HomePage.jsx
@@ -9,7 +9,11 @@ const HomePage = () => {
 		<Container maxW={"container.xl"} py={12}>
 			<VStack spacing={8}>
 			   <Text
-		
+					fontSize={"30"}
+					fontWeight={"bold"}
+					bgGradient={"linear(to-r, cyan.400, blue.500"}
+					textAlign={"center"}
+					bgClip={"text"}
 		       >
 			     Home Page 🚀
 		       </Text>
