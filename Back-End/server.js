662388c4fa32ace7b8fb023d6676f82a520ee11a
Chris Forrest <42876707+Chris-Forrest@users.noreply.github.com>
2025-02-11 10:55:14 -0500

    Update server.js
    setting project up for deployment

--- Back-End/server.js
+++ Back-End/server.js
@@ -2,15 +2,20 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001
 
+const _dirname = path.resolve();
+
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
+
+if(process.env.NODE_ENV === "production") {}
 
 app.listen(PORT, () => {
     connectDB();
