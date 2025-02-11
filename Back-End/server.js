6ee1e3c1f2b8e97c393f8493f2c122d7dec8fb4f
Chris Forrest <42876707+Chris-Forrest@users.noreply.github.com>
2025-02-11 11:01:28 -0500

    Update server.js
    adding code to run on deployment

--- Back-End/server.js
+++ Back-End/server.js
@@ -15,7 +15,12 @@ app.use(express.json());
 
 app.use("/api/products", productRoutes);
 
-if(process.env.NODE_ENV === "production") {}
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(_dirname, "/Front-End/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+    })
+};
 
 app.listen(PORT, () => {
     connectDB();
