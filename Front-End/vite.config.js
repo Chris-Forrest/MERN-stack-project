15bf6550e56a4114173f519dfd61976fed249213
Chris Forrest <42876707+Chris-Forrest@users.noreply.github.com>
2025-02-12 14:14:24 -0500

    deployment error
    trying to fix the deployment error to post a free version for use

--- Front-End/vite.config.js
+++ Front-End/vite.config.js
@@ -1,32 +1,4 @@
-import { dirname, resolve } from 'node:path'
-import { fileURLToPath } from 'node:url'
-import { defineConfig } from 'vite'
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
-
-export default defineConfig({
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'lib/main.js'),
-      name: 'MyLib',
-      // the proper extensions will be added
-      fileName: 'my-lib',
-    },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ['vue'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          vue: 'Vue',
-        },
-      },
-    },
-  },
-})
-/*
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -41,4 +13,3 @@ export default defineConfig({
     }
   }
 })
-*/
